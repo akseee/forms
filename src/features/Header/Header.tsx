b16a1b66ad055bf4ede4c7af778c8178ce87b688
Akseenja <klasik1995@bk.ru>
2025-08-23 15:44:12 +0300

    docs: upd

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -3,7 +3,15 @@ import styles from './Header.module.css';
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Character creator studio!</h1>
+      <h1 className={styles.title}>
+        Character creator studio!{' '}
+        <span className={styles.span}>
+          by{' '}
+          <a href="https://github.com/akseee/" target="_blank" rel="noreferrer">
+            @akseee
+          </a>
+        </span>
+      </h1>
       <p className={styles.text}>
         Create your character in two ways: with a regular form or with the
         super-convenient React Hook Form
